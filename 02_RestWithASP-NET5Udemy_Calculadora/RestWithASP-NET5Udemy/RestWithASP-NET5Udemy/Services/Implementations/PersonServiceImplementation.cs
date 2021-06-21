75572e013885930b83f4aa6e2bba1accf3bc6a1d
Adriano Zovka <adrianozovka@gmail.com>
2021-06-21 08:58:46 -0300

    Working four verbs 2

--- 02_RestWithASP-NET5Udemy_Calculadora/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Services/Implementations/PersonServiceImplementation.cs
+++ 02_RestWithASP-NET5Udemy_Calculadora/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Services/Implementations/PersonServiceImplementation.cs
@@ -1,4 +1,5 @@
 ﻿using RestWithASP_NET5Udemy.Model;
+using RestWithASP_NET5Udemy.Model.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,63 +10,92 @@ namespace RestWithASP_NET5Udemy.Services.Implementations
 {
     public class PersonServiceImplementation : IPersonService
     {
-        private volatile int count;
+        private MySQLContext _context;
 
-        public Person Create(Person person)
+        public PersonServiceImplementation(MySQLContext context)
         {
-            return person;
+            _context = context;
+
         }
 
-        public void Delete(long id)
-        {
-           
-        }
 
         public List<Person> FindAll()
         {
-            List<Person> persons = new List<Person>();
-            for(int i= 0; i<8; i++)
-            {
-                Person person = MockPerson(i);
-                persons.Add(person);
-            }
-
-            return persons;
+            return _context.Persons.ToList();
         }
+
 
         public Person FindByID(long id)
         {
-            return new Person
-            {
-                Id = IncrementANdGet(),
-                FirstName = "Adriano",
-                LastName = "Melo",
-                Address = "Recife, Pernambuco, Brazil",
-                Gender = "Male"
-            };
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
 
 
-        private Person MockPerson(int i)
-        {
-            return new Person
-            {
-                Id = IncrementANdGet(),
-                FirstName = "First Name" + i,
-                LastName = "Last Name" + i,
-                Address = "Some Address" + i,
-                Gender = "Male"
-            };
-        }
 
-        private long IncrementANdGet()
+        public Person Create(Person person)
         {
-            return Interlocked.Increment(ref count);
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+
+            return person;
         }
 
         public Person Update(Person person)
         {
+            if (!Exists(person.Id)) { return new Person(); }
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if (result != null)
+            {
+
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+
             return person;
+        }
+
+
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+
+
+            if (result != null)
+            {
+
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+
+
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
--- 02_RestWithASP-NET5Udemy_Calculadora/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Startup.cs
+++ 02_RestWithASP-NET5Udemy_Calculadora/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Startup.cs
@@ -2,10 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestWithASP_NET5Udemy.Model.Context;
 using RestWithASP_NET5Udemy.Services;
 using RestWithASP_NET5Udemy.Services.Implementations;
 using System;
@@ -29,6 +32,10 @@ namespace RestWithASP_NET5Udemy
         {
 
             services.AddControllers();
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
 
             services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
